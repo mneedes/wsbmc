3a57d6ce7575f418378d6dc7cc3db25d589e80b2
mneedes <mneedes@gmail.com>
2025-02-15 12:52:13 -0800

    Partial implementation of auto-discovery, WIP

--- README.md
+++ README.md
@@ -7,12 +7,14 @@ This is distributed under the Modified "shit-ware" license (see LICENSE.md).
 
 1. Python 3.10 or better (I refused to use python until they implemented a switch/case equivalent, and I waited a LONG time)
 2. You need "requests" so invoke 'pip3 install requests' if you don't have it!
+3. For auto-discovery to work you might need to allow broadcasts from your firewall,
+    if it doesn't work try the following: "sudo ufw allow 11430/udp"
+4. Note that auto-discovery is optional if you already know the IP of your BluOS device.
 
 ## Instructionz
 
-1. Modify wsbmc.py with the IP address of your BluOS hardware.
-     (I haven't implemented auto-discovery yet)
-2. Open a terminal and invoke python3 ./wsbmc.py
+1. Open a terminal and invoke "python3 ./wsbmc.py".
+2. Or, if you already know the IP address of the BluOS device, invoke "python ./wsbmc.py IPADDR", where IPADDR is your device's IP (e.g.: 192.168.1.110).
 3. The following key commands are supported by the player:
 
 - u - volume up (and unmute)
--- wsbmc.py
+++ wsbmc.py
@@ -12,13 +12,95 @@ import curses
 import requests
 # XML sucks and the name "xml.etree.ElementTree" makes me vomit.
 import xml.etree.ElementTree as ET
-# Wow, finally a good name, thank goodness at least someone has some sense.
-import time
+# Wow, finally some good names, thank goodness at least someone has some sense.
+import sys, time, random, struct, socket, ipaddress
 
-# Put your device's IP address here, auto-discovery is for LOOZZERS
-#    Ya know, I might add it later
-IP="192.168.1.212"
-baseURL="http://" + IP + ":11000/"
+IP_Broadcast="192.168.1.255"
+IP_Device="192.168.1.100"
+
+def get_subnet_mask(ip_address_with_prefix):
+    """
+    Extracts the subnet mask from an IP address string that includes a prefix length (CIDR notation).
+
+    Args:
+        ip_address_with_prefix: IP address and prefix length (e.g., "192.168.1.1/24").
+
+    Returns:
+        The subnet mask as a string (e.g., "255.255.255.0").
+        Returns None if the input is invalid.
+    """
+    try:
+        network = ipaddress.ip_network(ip_address_with_prefix, strict=False)
+        return str(network.netmask)
+    except ValueError:
+        return None
+
+# Example usage
+ip_address_with_prefix = "192.168.1.1/24"
+subnet_mask = get_subnet_mask(ip_address_with_prefix)
+
+if subnet_mask:
+    print(f"The subnet mask for {ip_address_with_prefix} is: {subnet_mask}")
+else:
+    print(f"Invalid IP address or format: {ip_address_with_prefix}")
+
+def GetBroadcastAddress():
+    network = ipaddress.ip_interface(f"{ip_address}/{subnet_mask}")
+    return str(network.network.broadcast_address)
+
+def LDSP_Add(data, addr):
+    None
+    # print(data)
+    # print(addr)
+
+print(socket.gethostbyname(''))
+
+def LDSP_Query(sock):
+    if not hasattr(LDSP_Query, "packet"):
+        # Construct the query packet
+        header  = struct.pack("!6s", b'\x06LSDP\x01')
+        message = struct.pack("!5s", b'\x05\x51\x01\x00\x01')
+        LDSP_Query.packet = header + message
+    # Send the packet and wait up to 750ms for response(s)
+    sock.sendto(LDSP_Query.packet, (IP_Broadcast, 11430))
+    timeout  = 0.750
+    doneTime = time.time() + timeout
+    while 1:
+        try:
+            sock.settimeout(timeout)
+            data, addr = sock.recvfrom(1024)
+            LDSP_Add(data, addr)
+        except socket.timeout:
+            None
+        except socket.error as e:
+            print(f"Socket error: {e}")
+            break
+        timeout = doneTime - time.time()
+        if timeout < 0:
+            break
+
+def LDSP_Discovery():
+    # Set up a UDP broadcast socket
+    sock  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((IP_Broadcast, 11430))
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    # Fire off the seven (Sieben, (Siete (6+1))) query packets
+    #   and obtain response(s)
+    fireTimes = [ 1, 2, 3, 5, 7, 10 ]
+    startTime = time.time()
+    #TODO:
+    #for p in range(6):
+    for p in range(3):
+        LDSP_Query(sock)
+        waitTime  = fireTimes[p] - (time.time() - startTime)
+        waitTime += 0.001 * random.randint(0, 250)
+        if waitTime > 0:
+            time.sleep(waitTime)
+    LDSP_Query(sock)
+    sock.close()
+    # TODO: Determine actual IP
+    return IP_Device
 
 def SendGetRequest(request):
     return requests.get(baseURL + request)
@@ -95,8 +177,16 @@ def Loop():
             # Update status if no key pressed during the "halfassdelay"
             RefreshStatus()
 
+if len(sys.argv) > 1:
+    IP_Device = sys.argv[1]
+else:
+    IP_Device = LDSP_Discovery()
+
+baseURL="http://" + IP_Device + ":11000/"
+stdscr = ScreenInit()
+
 try:
-    stdscr = ScreenInit()
+    #stdscr = ScreenInit()
     RefreshStatus()
     Loop()
 except:
