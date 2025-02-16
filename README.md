273d488fd45f2ec3212cd8437890a9930d9247c0
mneedes <mneedes@gmail.com>
2025-02-15 16:04:57 -0800

    Simple Auto-discovery working

--- README.md
+++ README.md
@@ -6,16 +6,17 @@ This is distributed under the Modified "shit-ware" license (see LICENSE.md).
 ## Prerequisitez
 
 1. Python 3.10 or better (I refused to use python until they implemented a switch/case equivalent, and I waited a LONG time)
-2. You need "requests" so invoke 'pip3 install requests' if you don't have it!
+2. You need "requests" and "netifaces" so invoke 'pip3 install requests netifaces' if you don't have them!
 3. For auto-discovery to work you might need to allow broadcasts from your firewall,
-    if it doesn't work try the following: "sudo ufw allow 11430/udp"
-4. Note that auto-discovery is optional if you already know the IP of your BluOS device.
+    if this is the case then try the following: "sudo ufw allow 11430/udp"
+4. Note that auto-discovery is optional if you already know the IP address of your BluOS device.
 
 ## Instructionz
 
 1. Open a terminal and invoke "python3 ./wsbmc.py".
-2. Or, if you already know the IP address of the BluOS device, invoke "python ./wsbmc.py IPADDR", where IPADDR is your device's IP (e.g.: 192.168.1.110).
-3. The following key commands are supported by the player:
+2. Or, if you already know the IP address of the BluOS device, invoke "python3 ./wsbmc.py IPADDR", where IPADDR is your device's IP (e.g.: 192.168.1.110).
+3. Or, if you only have one BluOS device, invoke "python ./wsbmc.py first" and it will connect to the first BluOS device it finds! There can only be one...
+4. The following key commands are supported by the player:
 
 - u - volume up (and unmute)
 - d - volume down
--- wsbmc.py
+++ wsbmc.py
@@ -5,71 +5,69 @@
 #    Created out of desperation due to lack of BluOS Linux Support
 #           version 0.0.0.0.0.-1
 #
+#  usage: python3 ./wsbmc.pl [first|IP_ADDRESS]
+#
 
 # Curse You He-Man !!!
 import curses
 # "requests" because REST requests are more important than anything else using requests!
 import requests
 # XML sucks and the name "xml.etree.ElementTree" makes me vomit.
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as vomit
+# "iface", is that something new from Apple? Why not netinterfaces or netif?
+import netifaces as fuckfaces
 # Wow, finally some good names, thank goodness at least someone has some sense.
-import sys, time, random, struct, socket, ipaddress
+import sys, time, random, struct, socket
 
-IP_Broadcast="192.168.1.255"
-IP_Device="192.168.1.100"
+global IP_Device
 
-def get_subnet_mask(ip_address_with_prefix):
-    """
-    Extracts the subnet mask from an IP address string that includes a prefix length (CIDR notation).
-
-    Args:
-        ip_address_with_prefix: IP address and prefix length (e.g., "192.168.1.1/24").
-
-    Returns:
-        The subnet mask as a string (e.g., "255.255.255.0").
-        Returns None if the input is invalid.
-    """
+def LDSP_Add(packet, address):
     try:
-        network = ipaddress.ip_network(ip_address_with_prefix, strict=False)
-        return str(network.netmask)
-    except ValueError:
+        # Filter out announce messages
+        if packet[0:6] == b'\x06LSDP\x01' and packet[7] == 65:
+            # Remove packet header
+            message    = packet[6:packet[6]]
+            hdrLength  = 3 + message[2]
+            hdrLength += message[hdrLength] + 2
+            count      = message[hdrLength - 1]
+            # remove announce message header
+            message = message[(hdrLength - 1):]
+            # TODO: more shit
+            for r in range(count):
+                None
+            IP_Device, port = address
+            return IP_Device
+    except:
         return None
 
-# Example usage
-ip_address_with_prefix = "192.168.1.1/24"
-subnet_mask = get_subnet_mask(ip_address_with_prefix)
-
-if subnet_mask:
-    print(f"The subnet mask for {ip_address_with_prefix} is: {subnet_mask}")
-else:
-    print(f"Invalid IP address or format: {ip_address_with_prefix}")
-
 def GetBroadcastAddress():
-    network = ipaddress.ip_interface(f"{ip_address}/{subnet_mask}")
-    return str(network.network.broadcast_address)
+    interfaces = fuckfaces.interfaces()
+    for interface in interfaces:
+        if interface != 'lo':
+            try:
+                addresses = fuckfaces.ifaddresses(interface)
+                return addresses[fuckfaces.AF_INET][0]['broadcast']
+            except KeyError:
+                pass
+    return None
 
-def LDSP_Add(data, addr):
-    None
-    # print(data)
-    # print(addr)
-
-print(socket.gethostbyname(''))
-
-def LDSP_Query(sock):
-    if not hasattr(LDSP_Query, "packet"):
+def LDSP_Query(sock, IP_Broadcast, useFirst):
+    if not hasattr(LDSP_Query, "txPacket"):
         # Construct the query packet
         header  = struct.pack("!6s", b'\x06LSDP\x01')
         message = struct.pack("!5s", b'\x05\x51\x01\x00\x01')
-        LDSP_Query.packet = header + message
-    # Send the packet and wait up to 750ms for response(s)
-    sock.sendto(LDSP_Query.packet, (IP_Broadcast, 11430))
+        LDSP_Query.txPacket = header + message
+    # Send the packet and wait for response(s)
+    sock.sendto(LDSP_Query.txPacket, (IP_Broadcast, 11430))
     timeout  = 0.750
     doneTime = time.time() + timeout
     while 1:
         try:
             sock.settimeout(timeout)
-            data, addr = sock.recvfrom(1024)
-            LDSP_Add(data, addr)
+            rxPacket, address = sock.recvfrom(1024)
+            IP_Address = LDSP_Add(rxPacket, address)
+            if IP_Address != None and useFirst:
+                return IP_Address
         except socket.timeout:
             None
         except socket.error as e:
@@ -78,32 +76,41 @@ def LDSP_Query(sock):
         timeout = doneTime - time.time()
         if timeout < 0:
             break
+    return None
 
-def LDSP_Discovery():
+def LDSP_Discovery(useFirst):
+    if not hasattr(LDSP_Query, "IP_Broadcast"):
+        LDSP_Discovery.IP_Broadcast = GetBroadcastAddress()
+        if LDSP_Discovery.IP_Broadcast == None:
+            raise Exception("Can't get broadcast address");
     # Set up a UDP broadcast socket
     sock  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((IP_Broadcast, 11430))
+    sock.bind((LDSP_Discovery.IP_Broadcast, 11430))
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     # Fire off the seven (Sieben, (Siete (6+1))) query packets
     #   and obtain response(s)
     fireTimes = [ 1, 2, 3, 5, 7, 10 ]
     startTime = time.time()
-    #TODO:
-    #for p in range(6):
-    for p in range(3):
-        LDSP_Query(sock)
+    for p in range(6):
+        IP_Address = LDSP_Query(sock, LDSP_Discovery.IP_Broadcast, useFirst)
+        if IP_Address != None and useFirst:
+            return IP_Address
         waitTime  = fireTimes[p] - (time.time() - startTime)
         waitTime += 0.001 * random.randint(0, 250)
         if waitTime > 0:
             time.sleep(waitTime)
-    LDSP_Query(sock)
+    IP_Address = LDSP_Query(sock, LDSP_Discovery.IP_Broadcast, useFirst)
+    if IP_Address != None and useFirst:
+        return IP_Address
     sock.close()
-    # TODO: Determine actual IP
-    return IP_Device
+    return None
 
 def SendGetRequest(request):
-    return requests.get(baseURL + request)
+    if not hasattr(SendGetRequest, "baseURL"):
+        global IP_Device
+        SendGetRequest.baseURL = "http://" + IP_Device + ":11000/"
+    return requests.get(SendGetRequest.baseURL + request)
 
 def VolumeUp(dB):
     SendGetRequest("Volume?db=" + dB);
@@ -116,7 +123,7 @@ def RefreshStatus():
         RefreshStatus.line = "<Nothing playing>"
     status = SendGetRequest("Status")
     try:
-        root   = ET.fromstring(status.text)
+        root   = vomit.fromstring(status.text)
         artist = root.find("artist").text;
         song   = root.find("name").text;
         RefreshStatus.line = artist + " : " + song
@@ -178,15 +185,15 @@ def Loop():
             RefreshStatus()
 
 if len(sys.argv) > 1:
-    IP_Device = sys.argv[1]
+    if sys.argv[1] == 'first':
+        IP_Device = LDSP_Discovery(True)
+    else:
+        IP_Device = sys.argv[1]
 else:
-    IP_Device = LDSP_Discovery()
-
-baseURL="http://" + IP_Device + ":11000/"
-stdscr = ScreenInit()
+     IP_Device = LDSP_Discovery(False)
 
 try:
-    #stdscr = ScreenInit()
+    stdscr = ScreenInit()
     RefreshStatus()
     Loop()
 except:
